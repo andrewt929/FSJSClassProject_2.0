717ec95c47ceb48b16f032dfce45296bdb721c1e
andrewt929 <andrewt929@gmail.com>
2018-03-20 00:56:11 -0400

    Added in the Path configuration & index.html

--- src/server.js
+++ src/server.js
@@ -3,10 +3,13 @@
 // Define the server's required resources
 const express = require('express');
 const config = require('./config');
+const path = require('path');
+const publicPath = path.resolve(__dirname, '../public');
 
 // Define an app
 const app = express();
 
+app.use(express.static(publicPath));
 app.use(function(req, res, next) {
     res.end("Hello World!");
 });
