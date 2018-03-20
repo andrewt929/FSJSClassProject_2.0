61b9093f9b46d97805b4f5983fcfa383caaf2d82
andrewt929 <andrewt929@gmail.com>
2018-03-20 01:39:41 -0400

    updated the router file

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,4 +6,25 @@ router.use('/doc', function(req, res, next) {
     res.end(`Documentation http://expressjs.com/`);
   });
   
+router.post('/file', function(req, res, next) {
+    const newId = '' + FILES.length;
+    const data = req.body;
+    data.id = newId;
+
+    FILES.push(data);
+    res.status(201).json(data);
+});
+
+router.put('/file/:fileId', function(req, res, next) {
+    const {fileId} = req.params;
+    const file = FILES.find(entry => entry.id === fileId);
+    if (!file) {
+      return res.status(404).end(`Could not find file '${fileId}'`);
+    }
+  
+    file.title = req.body.title;
+    file.description = req.body.description;
+    res.json(file);
+});
+
 module.exports = router;
