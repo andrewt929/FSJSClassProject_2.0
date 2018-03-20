65be4e0e7c7ca89556eaf4b271a1eb7cfb77343a
andrewt929 <andrewt929@gmail.com>
2018-03-20 00:45:18 -0400

    Created a basic app on the server & added comments

--- src/server.js
+++ src/server.js
@@ -1,4 +1,12 @@
 // src/server.js
 
+// Define the server's required resources
 const express = require('express');
 const config = require('./config');
+
+// Define an app
+const app = express();
+
+app.use(function(req, res, next) {
+    res.end("Hello World!");
+});
